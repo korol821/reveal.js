6f37eb6aa32358cacfee66dafb3a7d3e6e2716bd
Yana <137148789+korol821@users.noreply.github.com>
2023-11-24 14:02:17 +0200

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -295,3 +295,9 @@ gulp.task('test', gulp.series( 'eslint', 'qunit' ))
 gulp.task('default', gulp.series(gulp.parallel('js', 'css', 'plugins'), 'test'))
 
 gulp.task('build', gulp.parallel('js', 'css', 'plugins'))
+
+gulp.task('package', gulp.series(() =>
+
+    gulp.src(
+        [
+            './index.html',
